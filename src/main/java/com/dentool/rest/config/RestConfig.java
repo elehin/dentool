30e0188cb096059f4a951348cec7e32511a8c485
Rubén <ruben.martinez.garcia@outlook.com>
2016-04-30 21:16:53 +0200

    No needo to comment

---
+++ src/main/java/com/dentool/rest/config/RestConfig.java
@@ -0,0 +1,9 @@
+package com.dentool.rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestConfig extends Application {
+
+}
