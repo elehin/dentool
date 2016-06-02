7de58225295c45c4fed5c7fdefaea6919c8e4228
Rubén <ruben.martinez.garcia@outlook.com>
2016-06-02 21:15:30 +0200

    Pruebas cron

---
+++ src/main/java/com/dentool/cron/Test.java
@@ -0,0 +1,10 @@
+package com.dentool.cron;
+
+public class Test {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i + ". Se está ejecutando el cron ########################");
+		}
+	}
+
+}
