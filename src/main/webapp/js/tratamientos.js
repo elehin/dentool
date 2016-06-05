509cfce846de967640b3c343bd7718f34105f71c
Rubén <ruben.martinez.garcia@outlook.com>
2016-06-05 19:39:11 +0200

    Errores en urls y se quita el cron de OpenShift

--- src/main/webapp/js/tratamientos.js
+++ src/main/webapp/js/tratamientos.js
@@ -1,10 +1,10 @@
-var rootURL = 'https://dentool-elehin.rhcloud.com/service/paciente/';
+var rootURL = 'https://dentool-elehin.rhcloud.com/service/tratamiento/';
 var diagnosticoURL = 'https://dentool-elehin.rhcloud.com/service/diagnostico/';
 var tratamientoURL = 'https://dentool-elehin.rhcloud.com/service/tratamiento/';
 var tratamientosTopURL = 'https://dentool-elehin.rhcloud.com/service/tratamientoTop';
 var serverURL = 'https://dentool-elehin.rhcloud.com/';
 
-// var rootURL = 'http://localhost:8080/service/paciente/';
+// var rootURL = 'http://localhost:8080/service/tratamiento/';
 // var diagnosticoURL = 'http://localhost:8080/service/diagnostico/';
 // var tratamientoURL = 'http://localhost:8080/service/tratamiento/';
 // var tratamientosTopURL = 'http://localhost:8080/service/tratamientoTop';
